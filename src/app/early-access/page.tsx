9f68a9e74ac9c1e25dba0e63211224556dd1ebd5
gyauelvis <gyauelvis@gmail.com>
2025-04-30 22:32:01 +0000

    fixing build issues

--- src/app/early-access/page.tsx
+++ src/app/early-access/page.tsx
@@ -18,7 +18,7 @@ import {
 } from "@/components/ui/form"
 import { Input } from "@/components/ui/input"
 import { Textarea } from "@/components/ui/textarea"
-import { Check, ArrowRight, Car, Mail, User } from "lucide-react"
+import { Check, ArrowRight, Mail, User } from "lucide-react"
 
 const earlyAccessFormSchema = z.object({
     name: z.string().nonempty("First name is required"),
